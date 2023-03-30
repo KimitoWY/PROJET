bb6d534905153632f6737534afee31643ac01d9b
Théo <th.berroyer@gmail.com>
2023-03-30 12:12:01 +0200

    Création de produits dans la fonction load de VenteFixtures

--- src/DataFixtures/VenteFixtures.php
+++ src/DataFixtures/VenteFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Good;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,44 @@ class VenteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        /*
+         * Création des produits
+         * */
+
+        $good1 = new Good();
+        $good1
+            ->setLabel('Stylo plume')
+            ->setPrice(10.99)
+            ->setStock(10);
+        $manager->persist($good1);
+
+        $good2 = new Good();
+        $good2
+            ->setLabel('Règle 25cm')
+            ->setPrice(2.99)
+            ->setStock(30);
+        $manager->persist($good2);
+
+        $good3 = new Good();
+        $good3
+            ->setLabel('Cartouche d\'encre')
+            ->setPrice(0.99)
+            ->setStock(80);
+        $manager->persist($good3);
+
+        $good4 = new Good();
+        $good4
+            ->setLabel('Colle')
+            ->setPrice(4.99)
+            ->setStock(40);
+        $manager->persist($good4);
+
+        $good5 = new Good();
+        $good5
+            ->setLabel('Ciseaux')
+            ->setPrice(7.99)
+            ->setStock(20);
+        $manager->persist($good5);
 
         $manager->flush();
     }
