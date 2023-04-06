6a998dd1276860ead9c0c4e3e186315c3e9c4f25
Théo <th.berroyer@gmail.com>
2023-04-07 00:32:02 +0200

    Modif erreur nom d'un paramètre

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -18,7 +18,7 @@ class UserType extends AbstractType
         $builder
             ->add('login',
                 TextType::class, [
-                    'login' => 'Login',
+                    'label' => 'Login',
                     'required' => true
                 ])
             ->add('roles')
