fa9cff86525f0999adfbbc6482b446eed513a8d3
Théo <th.berroyer@gmail.com>
2023-04-07 00:21:51 +0200

    Modif paramètres des add du formulaire

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,31 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login')
+            ->add('login',
+                TextType::class, [
+                    'login' => 'Login',
+                    'required' => true
+                ])
             ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('birthday')
+            ->add('password',
+                RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'required' => true
+                ])
+            ->add('nom',
+                TextType::class, [
+                    'label' => 'Nom',
+                    'required' => true
+                ])
+            ->add('prenom',
+                TextType::class, [
+                    'label' => 'Prenom',
+                    'required' => true
+                ])
+            ->add('birthday',
+                DateType::class, [
+                    'year' => range(date('Y'), 1923)
+                ])
         ;
     }
 
